f733e19a71314ccf2181db1f81e8d9ccb68fc34c
magda3344 <magdalena.stelmach1@gmail.com>
2020-01-21 00:38:29 +0100

    praca domowa

--- src/praca domowa/PD1.js
+++ src/praca domowa/PD1.js
@@ -3,12 +3,18 @@ function arrayPD1() {
 var a=['jeden','dwa','trzy','cztery','pięć'];
 //var a=new Array('jeden','dwa','trzy','cztery','pięć'); //a- to jest cała tablica
 for(var b in a ){ //zmienna b w tablicy a, czyli każda z wartości w tablicy
+    //document.write(a[b]); //wyświetl każdą wartość b w tablicy a
+}
+    document.getElementById("demo").innerHTML = a;
 
-  //console.log(a[b]); //wyświetl każdą wartość b w tablicy a
+var wpisz = prompt("Wpisz coś:", "coś wpisałam");
+    var k=[wpisz,'dwa','trzy','cztery','pięć'];
+for(var c in k){
 }
-var wpisz = prompt ("Wpisz coś:", "coś wpisałam");
-document.getElementById("demo").innerHTML = a;
+    document.getElementById("demo1").innerHTML = k;
 }
+
 
 //a[0]= to pierwszy element tablicy
 //console.log(a[0]); - wyświetli sie tylko pierszwy element tablicy
+
